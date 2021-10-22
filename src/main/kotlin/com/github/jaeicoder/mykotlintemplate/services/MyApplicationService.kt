75a88907592aa937b7839072024d4ed3faff0db2
GitHub Action <action@github.com>
2021-10-22 11:21:40 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/jaeicoder/mykotlintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.jaeicoder.mykotlintemplate.services
+
+import com.github.jaeicoder.mykotlintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
